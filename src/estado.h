99bc6e0af29f8ee96e65e184bd7f5934b47dd8a8
CarlosHSF99 <a89509@alunos.uminho.pt>
2019-03-24 00:59:48 +0000

    Estrutua de dados expandida

--- src/estado.h
+++ src/estado.h
@@ -7,6 +7,7 @@
 
 #define DIM 8
 #define MAX_LENGTH 1024
+#define MAX_POS 64
 
 /*
 estado.h
@@ -16,11 +17,19 @@ Definição do estado i.e. tabuleiro. Representação matricial do tabuleiro.
 // definição de valores possiveis no tabuleiro
 typedef enum {VAZIA, VALOR_X, VALOR_O, VALOR_DOT, VALOR_HELP, END_OF_LINE} VALOR;
 
+// posicao
+typedef struct posicao {
+    int l, c;
+} POSICAO
+
 // Estrutura que armazena o estado do jogo
-typedef struct estado{
-    VALOR peca;         // peça do jogador que vai jogar!
-    VALOR grelha[8][8]; // grelha
-    char modo; // modo em que se está a jogar! 0-> manual, 1-> contra computador
+typedef struct estado {
+    char modo;                  // modo em que se está a jogar! 0-> manual, 1-> contra computador
+    VALOR peca;                 // peça do jogador que vai jogar!
+    VALOR grelha[DIM][DIM];     // grelha
+    POSICAO validas[MAX_POS]; // posiçoes validas
+    POSICAO alterar[MAX_POS];
+    POSICAO ajuda;              // posicao ajuda
 } ESTADO;
 
 void printg(ESTADO e);
