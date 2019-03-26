8a48916b2be07d95c84e68d92a26e7f3f2f58a31
kazoJP <zmpeixoto13@gmail.com>
2019-03-26 15:20:51 +0000

    vsHumans almost done

--- src/estado.h
+++ src/estado.h
@@ -46,7 +46,7 @@ void readFile (ESTADO *e, char *file_name);
 
 void writeFile(ESTADO *e, char *file_name);
 
-void play(int l, int c, ESTADO *e, char *file_name); 
+void play(int l, int c, ESTADO *e); 
 
 void something(ESTADO *e);
 
