c6ece5226430d4735c30bab4a5e3d369dbf0ebc5
CarlosHSF99 <a89509@alunos.uminho.pt>
2019-05-18 20:29:31 +0100

    Deleted unecessary comments

--- src/main.c
+++ src/main.c
@@ -5,37 +5,6 @@
  *
  * @return 
  */
-/*
-int main()
-{
-    LEST* ss = NULL;                               //
-    LEST s = calloc(1, sizeof(struct history));    // allocs memory to the stack setting every bit to zero
-    ESTADO e = {0};                                // declares a state "e" inicializing all its bits to zero
-
-    if (!s)                                        // checks if there was an error allocing memory to s
-        exit(0);                                   // terminates program if there was an error allocing memory
-    
-    e.modo = HELP;                                 // sets value of e.modo to ?
-    e.peca = HELP;                                 // sets value of e.peca to ?
-    e.botLVL = ' ';
-    e.bot = HELP;                                  //
-    
-    e.scoreX = 2;                                  // sets X score to 2
-    e.scoreO = 2;                                  // sets O score to 2
-    
-    boardInicial(e.grelha);                        // sets board to its inicial state
-    
-    s->e = e;                                      //
-    s->next = NULL;                                //
-    
-    interpreter(e, s);                             //
-    
-    ss = &s;                                       //
-    freeStack(ss);                                 //
-    
-    return 0;                                      //
-}
-*/
 int main()
 {
     LEST* ss = NULL;                               //
